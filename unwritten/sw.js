ed62e95c4219d425ed406b5fd24e39b9a0c611fe
justjude34-ship-it <justjude34@gmail.com>
2026-09-07 11:19:45 +1000

    Unwritten cover: add journal hero image above title

--- unwritten/sw.js
+++ unwritten/sw.js
@@ -1,4 +1,4 @@
-const C='unwritten-v5';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg','./cover.svg'])));self.skipWaiting();});
+const C='unwritten-v6';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./cover.svg','./manifest.webmanifest','./icon.svg'])));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(cache=>cache.put(e.request,c)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));});
